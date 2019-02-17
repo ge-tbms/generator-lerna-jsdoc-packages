cee40f8a9298f30260d79b4c1440d38b9e99fce1
dkypooh <18967131010@163.com>
2019-02-17 19:59:03 +0800

    add template

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,38 +1,126 @@
+/* eslint-disable */
 'use strict';
+
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const _ = require('lodash');
+const path = require('path');
+const fs = require('fs');
+
 
 module.exports = class extends Generator {
+  initializing() {
+    try {
+      this.username = process.env.USER || process.env.USERPROFILE.split(require('path').sep)[2]
+    } catch (e) {
+      this.username = ''
+    }
+  }
+
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(`Welcome to the phenomenal ${chalk.red('generator-lerna-jsdoc-packages')} generator!`)
+      yosay(`Welcome to the rad ${chalk.red('generator-typescript-jest-sdk')} generator!`)
     );
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        validate: name => {
+          if (!name) {
+            return 'Project name cannot be empty'
+          }
+          if (!/\w+/.test(name)) {
+            return 'Project name should only consist of 0~9, a~z, A~Z, _, .'
+          }
+
+          const fs = require('fs')
+          if (!fs.existsSync(this.destinationPath(name))) {
+            return true;
+          }
+          if (fs.statSync(this.destinationPath(name)).isDirectory()) {
+            return 'Project already exist'
+          }
+          return true
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Your project description',
+        default: ''
+      },
+      {
+        type: 'input',
+        name: 'keywords',
+        message: 'Your project keywords',
+        default: ''
+      },
+      {
+        type: 'input',
+        name: 'username',
+        message: 'Your name',
+        default: this.username
+      },
+      {
+        type: 'list',
+        name: 'registry',
+        message: 'Which registry would you use?',
+        choices: [
+          'https://registry.npm.taobao.org',
+          'https://registry.npmjs.org'
+        ]
       }
     ];
 
-    return this.prompt(prompts).then(props => {
+    return this.prompt(prompts).then(answers => {
       // To access props later use this.props.someAnswer;
-      this.props = props;
+      const keywords = answers.keywords;
+      this.answers = answers;
+      this.obj = {answers: this.answers};
+
     });
   }
 
+  configuring(answers) {
+
+    const done = this.async()
+    fs.exists(this.destinationPath(this.answers.name), exists => {
+      if (exists && fs.statSync(this.destinationPath(this.answers.name)).isDirectory()) {
+        this.log.error('Directory [' + this.answers.name + '] exists')
+        process.exit(1)
+      }
+      this.destinationRoot(path.join(this.destinationRoot(), this.answers.name))
+      done()
+    })
+  }
+
+
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+
+
+    this.fs.copyTpl(this.templatePath('packages'), this.destinationPath('packages'));
+
+
+    this.fs.copy(this.templatePath('conf.json'), this.destinationPath('conf.json'))
+    this.fs.copy(this.templatePath('lerna.json'), this.destinationPath('lerna.json'))
+    this.fs.copyTpl(this.templatePath('package.json_vm'), this.destinationPath('package.json'), this.obj)
+
+    this.fs.copyTpl(this.templatePath('readme.md'), this.destinationPath('readme.md'), this.obj)
+
   }
 
   install() {
-    this.installDependencies();
+    this.npmInstall(undefined, {
+      registry: this.answers.registry
+    })
+  }
+
+  end() {
+    this.log.ok('Project ' + this.answers.name + ' generated!!!')
+    this.spawnCommand('npm', ['run', 'bootstrap'])
   }
 };
